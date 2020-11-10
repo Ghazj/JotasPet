1846e2b9b519784a6065ba153ef3550b9e44d569
ghazj <ghazijj98@gmail.com>
2020-11-10 15:48:16 -0300

    Se crean archivos y carpetas de la parte backend del proyecto. se instalan dependencias. se desarolla el index, las routes, y el schema de la base de datos, se hace la conexión con esa. toda funciona ok

---
+++ src/backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const { mongoose } = require('./database.js');
+
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+ app.use(morgan('dev'));
+ app.use(express.json())
+
+//Routes
+app.use('/api/turnos',require('./routes/turnos.routes.js'));
+
+
+//statics files
+
+
+//starting the server
+app.listen(app.get('port'), ()=>{
+    console.log('server on port ' + String(app.get('port')));
+})
---
+++ src/backend/routes/turnos.routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Turnos = require('../models/turnos.js');
+
+router.get('/', async (req, res)=>{
+   const turnos = await Turnos.find();
+   console.log(turnos)
+   res.json(turnos)
+});
+
+router.get('/:id', async (req, res)=>{
+    const turnos = await Turnos.findById(req.params.id);
+    console.log(turnos)
+    res.json(turnos)
+ });
+
+router.post('/', async (req, res)=>{
+    await Turnos.create(req.body)
+    res.json({status: 'Turno Recibido'})
+})
+
+router.put('/:id', async (req, res) =>{
+    await Turnos.findByIdAndUpdate(req.params.id ,req.body)
+    res.json({status: 'Turno actualizado'})
+})
+
+router.delete('/:id', async (req, res) =>{
+    await Turnos.findByIdAndRemove(req.params.id)
+    res.json({status: 'Turno cancelado'})
+})
+
+module.exports = router;
